6c989fe9de77154c13ce24ec775193a04bb20f54
Matt Fleming <matthewfleming8@gmail.com>
2020-03-31 13:47:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  ["kiwi"].join
+  fruits = ["kiwi"].join
+  fruits << "durian"
+  fruits.join(" and ")
 end
 
-def oxford_comma(array)
-  ["kiwi", "durian"].join(" and ")
-end
+
 
