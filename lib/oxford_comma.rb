6d76da3a62dcf9d3a6846a3b1c5afd4e69005847
Matt Fleming <matthewfleming8@gmail.com>
2020-03-31 13:59:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length == 1 
     return "#{[0]}"
-    elsif array.length == 2 
-      return array.join(" and ")
+  elsif array.length == 2 
+    return array.join(" and ")
       
   end
 end
